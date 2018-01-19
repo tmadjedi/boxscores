37d03db48e8c497062e98cc34af5d47f3d6c9a85
Tom <tmadjedi@gmail.com>
2018-01-18 20:42:42 -0800

    Added basic scoreboard

--- boxscores.py
+++ boxscores.py
@@ -3,10 +3,20 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def boxscore():
+@app.route('/', defaults={'date': None})
+@app.route('/<date>')
+def show_scoreboard(date):
+    json = get_scoreboard_json(date)
+    indexes = [2, 4]
+    rows = json['resultSets'][1]['rowSet']
+    games = list(set([row[2] for row in rows]))
+
+    return render_template('scoreboard.html', games=games)
+
+@app.route('/boxscore/<gameid>')
+def show_boxscore(gameid):
     boxscore = []
-    json = get_boxscore_json()
+    json = get_boxscore_json(gameid)
     header = clean_boxscore_row(json['resultSets'][0]['headers'])
 
     for player_row in json['resultSets'][0]['rowSet']:
@@ -15,11 +25,17 @@ def boxscore():
     return render_template('boxscore.html', header=header, boxscore=boxscore)
 
 # eventually this will pull json directly from the nba api
-def get_boxscore_json():
+def get_boxscore_json(gameid):
     with open('static/0021600622_boxscore.json', 'r') as f:
         boxscore = json.load(f)
 
     return boxscore
+
+def get_scoreboard_json(date):
+    with open('static/01182018_scoreboard.json', 'r') as f:
+        scoreboard = json.load(f)
+
+    return scoreboard
 
 def clean_boxscore_row(row):
     cols = [0, 1, 3, 4, 6, 7]
